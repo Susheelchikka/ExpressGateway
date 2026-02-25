64989d25477cd6d1d8db4b537c2131ae832233f8
Susheelchikka <chikkasusheelkumar@gmail.com>
2026-02-25 17:31:07 +0530

    express gateway with redis

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ redisClient.connect().then(() => console.log('[Redis] Connected Successfully')).
  */
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each tenant to 100 requests per windowMs
+    max: 50, // limit each tenant to 100 requests per windowMs
     store: new RedisStore({
         sendCommand: (...args) => redisClient.sendCommand(args),
     }),
